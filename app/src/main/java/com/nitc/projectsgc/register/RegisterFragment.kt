11c451eb498007989918f88fb250368e207fddf6
Gyana <gyanaranjansahoo509@gmail.com>
2023-03-14 18:15:47 +0530

    Email Domain verification and user registration to the database added

--- app/src/main/java/com/nitc/projectsgc/register/RegisterFragment.kt
+++ app/src/main/java/com/nitc/projectsgc/register/RegisterFragment.kt
@@ -13,67 +13,75 @@ import androidx.navigation.fragment.findNavController
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.nitc.projectsgc.R
+import com.nitc.projectsgc.Students
 import com.nitc.projectsgc.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment(), AdapterView.OnItemSelectedListener {
-    lateinit var binding: FragmentRegisterBinding
+    lateinit var registerBinding: FragmentRegisterBinding
     lateinit var studentGender:Spinner
     lateinit var selectedGenderTextView: String
+
+//    creating database reference object
     var database : FirebaseDatabase = FirebaseDatabase.getInstance()
-    private val reference : DatabaseReference = database.reference.child("students")
+    private var reference : DatabaseReference = database.reference.child("students")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentRegisterBinding.inflate(inflater,container,false)
+        registerBinding = FragmentRegisterBinding.inflate(inflater,container,false)
         val arrayAdapter = ArrayAdapter.createFromResource(
             requireContext(),
             R.array.gender_options,
             android.R.layout.simple_spinner_item
         )
-        studentGender = binding.genderSpinnerInRegisterFragment
+        studentGender = registerBinding.genderSpinnerInRegisterFragment
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         studentGender.adapter = arrayAdapter
         studentGender.onItemSelectedListener = this
 
-        binding.signUpButtonInRegisterFragment.setOnClickListener{
-            val nameInput = binding.nameFieldInRegisterFragment.text.toString()
-            val emailInput = binding.emailFieldInRegisterFragment.text.toString()
-            val passwordInput = binding.passwordFieldInRegisterFragment.text.toString()
-            var phoneNumber = binding.phoneNumberInRegisterFragment.text.toString()
-            val dateOfBirth = binding.dateOfBirthInRegisterFragment.dayOfMonth.toString()+"/"+binding.dateOfBirthInRegisterFragment.month.toString()+"/"+binding.dateOfBirthInRegisterFragment.year.toString()
+        registerBinding.signUpButtonInRegisterFragment.setOnClickListener{
+            val nameInput = registerBinding.nameFieldInRegisterFragment.text.toString()
+            val emailInput = registerBinding.emailFieldInRegisterFragment.text.toString()
+            val passwordInput = registerBinding.passwordFieldInRegisterFragment.text.toString()
+            var phoneNumber = registerBinding.phoneNumberInRegisterFragment.text.toString()
+            val dateOfBirth = registerBinding.dateOfBirthInRegisterFragment.dayOfMonth.toString()+"/"+registerBinding.dateOfBirthInRegisterFragment.month.toString()+"/"+registerBinding.dateOfBirthInRegisterFragment.year.toString()
 
             if(nameInput.isEmpty()){
-                binding.nameFieldInRegisterFragment.error = "Name field cannot be empty"
-                binding.nameFieldInRegisterFragment.requestFocus()
+                registerBinding.nameFieldInRegisterFragment.error = "Name field cannot be empty"
+                registerBinding.nameFieldInRegisterFragment.requestFocus()
                 return@setOnClickListener
             }
             if(emailInput.isEmpty()){
-                binding.emailFieldInRegisterFragment.error = "Email field cannot be empty"
-                binding.emailFieldInRegisterFragment.requestFocus()
+                registerBinding.emailFieldInRegisterFragment.error = "Email field cannot be empty"
+                registerBinding.emailFieldInRegisterFragment.requestFocus()
+                return@setOnClickListener
+            }
+            if(!checkDomain(emailInput)){
+                registerBinding.emailFieldInRegisterFragment.error = "Email should be a valid nitc email"
+                registerBinding.emailFieldInRegisterFragment.requestFocus()
                 return@setOnClickListener
             }
             if(passwordInput.length < 8){
-                binding.passwordFieldInRegisterFragment.error = "Password should contain at least 8 characters"
-                binding.passwordFieldInRegisterFragment.requestFocus()
+                registerBinding.passwordFieldInRegisterFragment.error = "Password should contain at least 8 characters"
+                registerBinding.passwordFieldInRegisterFragment.requestFocus()
                 return@setOnClickListener
             }
 
             phoneNumber = phoneNumber.trim()
             if(phoneNumber.length <10 || phoneNumber.length >10){
                 phoneNumber.trim()
-                binding.phoneNumberInRegisterFragment.error = "Phone number should be 10 digits only"
-                binding.phoneNumberInRegisterFragment.requestFocus()
+                registerBinding.phoneNumberInRegisterFragment.error = "Phone number should be 10 digits only"
+                registerBinding.phoneNumberInRegisterFragment.requestFocus()
                 return@setOnClickListener
             }else if (!isDigitsOnly(phoneNumber)){
-                binding.phoneNumberInRegisterFragment.setText("")
+                registerBinding.phoneNumberInRegisterFragment.setText("")
                 Toast.makeText(context,"Oops !! you entered phone number in wrong format", Toast.LENGTH_LONG).show()
-                binding.phoneNumberInRegisterFragment.requestFocus()
+                registerBinding.phoneNumberInRegisterFragment.requestFocus()
                 return@setOnClickListener
             }
 
-            val checked = binding.tncBoxInRegisterFragment
+            val checked = registerBinding.tncBoxInRegisterFragment
             if(!checked.isChecked){
                 Toast.makeText(context,"You have to accept the terms and conditions", Toast.LENGTH_LONG).show()
                 return@setOnClickListener
@@ -87,45 +95,40 @@ class RegisterFragment : Fragment(), AdapterView.OnItemSelectedListener {
                     "D.O.B = $dateOfBirth\n"+
                     "Gender = $selectedGenderTextView \n"
                 , Toast.LENGTH_LONG).show()
-            reference.child("name").setValue(nameInput)
-            reference.child("dateOfBirth").setValue(dateOfBirth)
-            reference.child("emailId").setValue(emailInput)
-            reference.child("Student_id").setValue(emailInput.split("_")[1].subSequence(0,8).toString())
-            reference.child("phoneNumber").setValue(phoneNumber)
-            reference.child("gender").setValue(selectedGenderTextView)
-            reference.child("password").setValue(passwordInput)
 
+            val rollNo = emailInput.substring(emailInput.indexOf("_") + 1, emailInput.indexOf("@"))
 
-//            val fragManager = requireActivity().supportFragmentManager
-//            val transaction = fragManager.beginTransaction()
-//            transaction.replace(
-//                R.id.frameMain,
-//                BookingFragment()
-//            )
-//            transaction.addToBackStack(null) // if u want this fragment to stay in stack specify it
-//            transaction.commit()
+            val student = Students(rollNo,nameInput,dateOfBirth,emailInput,selectedGenderTextView,passwordInput,phoneNumber)
+
+            reference.child(rollNo).setValue(student).addOnCompleteListener{ task ->
+                if(task.isSuccessful){
+                    Toast.makeText(requireContext(),"The student has been added to the database",Toast.LENGTH_SHORT).show()
+                    findNavController().navigate(R.id.loginFragment)
+                }else{
+                    Toast.makeText(requireContext(),task.exception.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            findNavController().navigate(R.id.bookingFragment)
 
         }
 
-        binding.signInButtonInRegisterFragment.setOnClickListener{
-//            val fragManager = requireActivity().supportFragmentManager
-//            val transaction = fragManager.beginTransaction()
-//            transaction.replace(
-//                R.id.frameMain,
-//                SignInFragment()
-//            )
-//            transaction.addToBackStack(null) // if u want this fragment to stay in stack specify it
-//            transaction.commit()
-
+        registerBinding.signInButtonInRegisterFragment.setOnClickListener{
             findNavController().navigate(R.id.loginFragment)
-
-
-
         }
         // Inflate the layout for this fragment
-        return binding.root
+        return registerBinding.root
     }
 
+    private fun checkDomain(emailInput: String): Boolean {
+        val domain : String = emailInput.substring(emailInput.indexOf("@")+1,emailInput.length)
+        if(domain != "nitc.ac.in")return false
+        return  true
+    }
+
+    fun addStudentsToDatabase(){
+
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
--- app/src/main/java/com/nitc/projectsgc/signin/SignInFragment.kt
+++ app/src/main/java/com/nitc/projectsgc/signin/SignInFragment.kt
@@ -2,6 +2,7 @@ package com.nitc.projectsgc.signin
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract.Data
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,17 +12,18 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.nitc.projectsgc.R
 import com.nitc.projectsgc.SignUpActivity
 import com.nitc.projectsgc.booking.BookingFragment
-import com.nitc.projectsgc.databinding.FragmentSignInBinding
+import com.nitc.projectsgc.databinding.FragmentLoginBinding
 import com.nitc.projectsgc.register.RegisterFragment
 
 class SignInFragment : Fragment() {
 
-    private lateinit var binding: FragmentSignInBinding
+    private lateinit var binding: FragmentLoginBinding
+    var database : FirebaseDatabase = FirebaseDatabase.getInstance()
+    var reference : DatabaseReference = database.reference
 
 
     override fun onCreateView(
@@ -29,7 +31,7 @@ class SignInFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentSignInBinding.inflate(inflater, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -52,7 +54,18 @@ class SignInFragment : Fragment() {
             }
 
             Toast.makeText(requireContext(),"Email entered is $emailInput and password entered is $passwordInput",Toast.LENGTH_LONG).show()
+            reference.addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+//                    perform data retrieving in this method
+                    var userName : String = snapshot.child ("students").child("name").value as String
+                    Toast.makeText(requireContext(),"Name is right and name is $userName",Toast.LENGTH_LONG).show()
+                }
 
+                override fun onCancelled(error: DatabaseError) {
+//                    action taken if data not retrieved
+                }
+
+            })
 //            val fragManager = requireActivity().supportFragmentManager
 //            val transaction = fragManager.beginTransaction()
 //            transaction.replace(
