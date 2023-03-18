9f1c1fbe6a59590f99ef92e3d1ae77d60258167b
Prashant <parshantrathee1020@gmail.com>
2023-03-18 20:56:57 +0530

    Student card

--- app/src/main/java/com/nitc/projectsgc/adapters/StudentsAdapter.kt
+++ app/src/main/java/com/nitc/projectsgc/adapters/StudentsAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.nitc.projectsgc.R
@@ -12,10 +13,15 @@ import com.nitc.projectsgc.Students
 class StudentsAdapter(
     var context: Context,
     var isAdmin:Boolean,
-    var students:Array<Students>
+    var students:ArrayList<Students>
 ): RecyclerView.Adapter<StudentsAdapter.StudentsViewHolder>() {
     class StudentsViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
-        var studentNameText = itemView.findViewById<TextView>(R.id.nameInStudentCard)
+        var nameText = itemView.findViewById<TextView>(R.id.nameInStudentCard)
+        var dobText = itemView.findViewById<TextView>(R.id.dobInStudentCard)
+        var phoneText = itemView.findViewById<TextView>(R.id.phoneInStudentCard)
+        var rollText = itemView.findViewById<TextView>(R.id.rollNoInStudentCard)
+        var viewAppointmentsButton = itemView.findViewById<Button>(R.id.viewAppointmentsButtonInStudentCard)
+        var deleteButton = itemView.findViewById<Button>(R.id.deleteButtonInStudentCard)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentsViewHolder {
@@ -24,11 +30,11 @@ class StudentsAdapter(
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return students.size
     }
 
     override fun onBindViewHolder(holder: StudentsViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
     }
 
 
