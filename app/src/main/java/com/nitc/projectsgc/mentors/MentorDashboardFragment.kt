86d6cea4b060971b877d5428f91009394a247a67
Gyana <gyanaranjansahoo509@gmail.com>
2023-03-20 15:44:28 +0530

    some changes

--- app/src/main/java/com/nitc/projectsgc/mentors/MentorDashboardFragment.kt
+++ app/src/main/java/com/nitc/projectsgc/mentors/MentorDashboardFragment.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.inflate
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.nitc.projectsgc.R
+import com.nitc.projectsgc.SharedViewModel
+import com.nitc.projectsgc.databinding.FragmentMentorDashboardBinding
 
 
 class MentorDashboardFragment : Fragment() {
-
+    private val sharedViewModel : SharedViewModel  by activityViewModels()
+    lateinit var mentorDashboardBinding: FragmentMentorDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,7 +24,10 @@ class MentorDashboardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mentor_dashboard, container, false)
+        mentorDashboardBinding = FragmentMentorDashboardBinding.inflate(inflater,container,false)
+
+        var mentorLive = context?.let {  }
+
+        return mentorDashboardBinding.root
     }
 }
