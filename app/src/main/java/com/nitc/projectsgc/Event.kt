762c941b7e82e59a10a82e8d5223859cd1e65532
Prashant <parshantrathee1020@gmail.com>
2023-04-04 17:05:57 +0530

    Student card

--- app/src/main/java/com/nitc/projectsgc/Event.kt
+++ app/src/main/java/com/nitc/projectsgc/Event.kt
@@ -2,11 +2,11 @@ package com.nitc.projectsgc
 
 data class Event(
     var id:String = "",
-    var heading:String,
-    var venue:String,
-    var eventDate:String,
-    var eventTime:String,
+    var heading:String? = null,
+    var venue:String? = null,
+    var eventDate:String? = null,
+    var eventTime:String? = null,
     var link:String = "",
-    var publishDate:String,
-    var mentorName:String
+    var publishDate:String? = null,
+    var mentorName:String? = null
 )
