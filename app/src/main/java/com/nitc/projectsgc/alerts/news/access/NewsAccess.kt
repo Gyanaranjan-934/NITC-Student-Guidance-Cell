82e49345f79ee563b46c43f53400e39062069040
Prashant <parshantrathee1020@gmail.com>
2023-04-04 23:21:53 +0530

    Student card

--- app/src/main/java/com/nitc/projectsgc/alerts/news/access/NewsAccess.kt
+++ app/src/main/java/com/nitc/projectsgc/alerts/news/access/NewsAccess.kt
@@ -1,8 +1,12 @@
 package com.nitc.projectsgc.alerts.news.access
 
 import android.content.Context
+import android.renderscript.Sampler.Value
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.nitc.projectsgc.News
 import com.nitc.projectsgc.SharedViewModel
 import kotlin.coroutines.resume
@@ -30,5 +34,33 @@ class NewsAccess(
             }
         }
     }
+
+
+    suspend fun getNews():ArrayList<News>?{
+        return suspendCoroutine { continuation ->
+            var database = FirebaseDatabase.getInstance()
+            var reference = database.reference.child("news")
+            var newsArray = arrayListOf<News>()
+            reference.addListenerForSingleValueEvent(object:ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for ( ds in snapshot.children){
+                        var news = ds.getValue(News::class.java)
+                        if(news != null) newsArray.add(news)
+                        else {
+                            continuation.resume(null)
+                            return
+                        }
+                    }
+                    continuation.resume(newsArray)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    continuation.resume(null)
+                }
+
+            })
+        }
+    }
+
 
 }
--- app/src/main/java/com/nitc/projectsgc/alerts/news/fragments/AllNewsFragment.kt
+++ app/src/main/java/com/nitc/projectsgc/alerts/news/fragments/AllNewsFragment.kt
@@ -1,13 +1,23 @@
 package com.nitc.projectsgc.alerts.news.fragments
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.nitc.projectsgc.R
 import com.nitc.projectsgc.SharedViewModel
+import com.nitc.projectsgc.alerts.news.access.NewsAccess
 import com.nitc.projectsgc.databinding.FragmentAllNewsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 class AllNewsFragment:Fragment() {
 
@@ -21,6 +31,30 @@ class AllNewsFragment:Fragment() {
     ): View? {
         binding = FragmentAllNewsBinding.inflate(inflater,container,false)
 
+        if(sharedViewModel.userType == "Mentor"){
+            binding.addNewsButtonInAllNewsFragment.visibility = View.VISIBLE
+        }else binding.addNewsButtonInAllNewsFragment.visibility = View.GONE
+        var coroutineScope = CoroutineScope(Dispatchers.Main)
+        val loadingDialog = Dialog(requireContext())
+        loadingDialog.setContentView(requireActivity().layoutInflater.inflate(R.layout.loading_dialog,null))
+        loadingDialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        coroutineScope.launch {
+            loadingDialog.show()
+            val news = NewsAccess(requireContext(),sharedViewModel,this@AllNewsFragment).getNews()
+            if(news != null){
+                if(news.isEmpty()){
+                    Toast.makeText(context,"No news found. Try again",Toast.LENGTH_SHORT).show()
+                    coroutineScope.cancel()
+                    loadingDialog.cancel()
+                }
+            }else{
+                Toast.makeText(context,"Some error occurred. Try again",Toast.LENGTH_SHORT).show()
+                coroutineScope.cancel()
+                loadingDialog.cancel()
+            }
+
+
+        }
 
         return binding.root
     }
