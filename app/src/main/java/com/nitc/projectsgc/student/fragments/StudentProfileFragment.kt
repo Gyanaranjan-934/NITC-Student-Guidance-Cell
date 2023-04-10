dc417b4a67edbeb36a7a0c0e72434df0dbbf622d
Gyana <gyanaranjansahoo509@gmail.com>
2023-04-10 16:20:14 +0530

    student update profile fixed

--- app/src/main/java/com/nitc/projectsgc/student/fragments/StudentProfileFragment.kt
+++ app/src/main/java/com/nitc/projectsgc/student/fragments/StudentProfileFragment.kt
@@ -28,10 +28,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
-class StudentProfileFragment: Fragment() {
+class StudentProfileFragment : Fragment() {
 
-    private val sharedViewModel:SharedViewModel by activityViewModels()
-    lateinit var binding:FragmentStudentProfileBinding
+    private val sharedViewModel: SharedViewModel by activityViewModels()
+    lateinit var binding: FragmentStudentProfileBinding
     lateinit var studentLive: MutableLiveData<Student?>
     var oldPassword = "NA"
     override fun onCreateView(
@@ -39,56 +39,61 @@ class StudentProfileFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentStudentProfileBinding.inflate(inflater,container,false)
-        if(sharedViewModel.userType == "Student" || sharedViewModel.userType == "Admin") {
+        binding = FragmentStudentProfileBinding.inflate(inflater, container, false)
+        if (sharedViewModel.userType == "Student" || sharedViewModel.userType == "Admin") {
 
-            if(sharedViewModel.userType == "Admin")  {
-                studentLive = StudentsAccess(requireContext(),this).getStudent(sharedViewModel.idForStudentProfile)
+            if (sharedViewModel.userType == "Admin") {
+                studentLive = StudentsAccess(
+                    requireContext(),
+                    this
+                ).getStudent(sharedViewModel.idForStudentProfile)
+            } else {
+                studentLive =
+                    StudentsAccess(requireContext(), this).getStudent(sharedViewModel.currentUserID)
             }
-            else {
-                studentLive = StudentsAccess(requireContext(),this).getStudent(sharedViewModel.currentUserID)
-            }
-            studentLive.observe(viewLifecycleOwner) { student ->
-                if(student != null) {
-                    binding.headingTVStudentProfileFragment.text = student.name
-                    binding.nameFieldStudentProfileFragment.setText(student.name)
-                    binding.emailFieldStudentProfileFragment.setText(student.emailId)
-                    binding.phoneNumberStudentProfileFragment.setText(student.phoneNumber)
-                    binding.passwordFieldStudentProfileFragment.setText(student.password)
-                    binding.emailFieldStudentProfileFragment.isEnabled = false
-                    oldPassword = student.password
 
+            if (studentLive != null) {
+                binding.headingTVStudentProfileFragment.text = sharedViewModel.currentStudent.name
+                binding.nameFieldStudentProfileFragment.setText(sharedViewModel.currentStudent.name)
+                binding.emailFieldStudentProfileFragment.setText(sharedViewModel.currentStudent.emailId)
+                binding.phoneNumberStudentProfileFragment.setText(sharedViewModel.currentStudent.phoneNumber)
+                binding.passwordFieldStudentProfileFragment.setText(sharedViewModel.currentStudent.password)
+                binding.emailFieldStudentProfileFragment.isEnabled = false
+                oldPassword = sharedViewModel.currentStudent.password
 
-
-
-                }else Toast.makeText(context,"Some error occurred. Try again",Toast.LENGTH_SHORT).show()
             }
             binding.updateButtonStudentProfileFragment.setOnClickListener {
                 val studentName = binding.nameFieldStudentProfileFragment.text.toString()
                 var studentPhone = binding.phoneNumberStudentProfileFragment.text.toString()
                 val studentPassword = binding.passwordFieldStudentProfileFragment.text.toString()
 
-                if(studentName.isEmpty()){
+                if (studentName.isEmpty()) {
                     binding.nameFieldStudentProfileFragment.error = "Name field cannot be empty"
                     binding.nameFieldStudentProfileFragment.requestFocus()
                     return@setOnClickListener
                 }
 
-                if(studentPassword.length < 8){
-                    binding.passwordFieldStudentProfileFragment.error = "Password should contain at least 8 characters"
+                if (studentPassword.length < 8) {
+                    binding.passwordFieldStudentProfileFragment.error =
+                        "Password should contain at least 8 characters"
                     binding.passwordFieldStudentProfileFragment.requestFocus()
                     return@setOnClickListener
                 }
 
                 studentPhone = studentPhone.trim()
-                if(studentPhone.length <10 || studentPhone.length >10){
+                if (studentPhone.length < 10 || studentPhone.length > 10) {
                     studentPhone.trim()
-                    binding.phoneNumberStudentProfileFragment.error = "Phone number should be 10 digits only"
+                    binding.phoneNumberStudentProfileFragment.error =
+                        "Phone number should be 10 digits only"
                     binding.phoneNumberStudentProfileFragment.requestFocus()
                     return@setOnClickListener
-                }else if (!isDigitsOnly(studentPhone)){
+                } else if (!isDigitsOnly(studentPhone)) {
                     binding.phoneNumberStudentProfileFragment.setText("")
-                    Toast.makeText(context,"Oops !! you entered phone number in wrong format", Toast.LENGTH_LONG).show()
+                    Toast.makeText(
+                        context,
+                        "Oops !! you entered phone number in wrong format",
+                        Toast.LENGTH_LONG
+                    ).show()
                     binding.phoneNumberStudentProfileFragment.requestFocus()
                     return@setOnClickListener
                 }
@@ -106,9 +111,10 @@ class StudentProfileFragment: Fragment() {
                     loadingDialog.create()
                     loadingDialog.show()
 
-                    studentLive.observe(viewLifecycleOwner) { student ->
-                        if (student != null) {
+
+                    if (studentLive != null) {
 //                val updatedMentor = BasicStudentAccess(requireContext()).updateStudent(student,oldPassword)
+                        var student = Student(sharedViewModel.currentStudent.rollNo,studentName,sharedViewModel.currentStudent.dateOfBirth,sharedViewModel.currentStudent.emailId,sharedViewModel.currentStudent.gender,studentPassword,studentPhone)
                             val database: FirebaseDatabase = FirebaseDatabase.getInstance()
                             val reference: DatabaseReference = database.reference.child("students")
                             val auth: FirebaseAuth = FirebaseAuth.getInstance()
@@ -138,6 +144,7 @@ class StudentProfileFragment: Fragment() {
                                             "Profile updated successfully",
                                             Toast.LENGTH_SHORT
                                         ).show()
+                                        binding.headingTVStudentProfileFragment.text = studentName
                                     } else {
                                         Toast.makeText(
                                             context,
@@ -151,13 +158,12 @@ class StudentProfileFragment: Fragment() {
                             updateCoroutineScope.cancel()
 
                         }
-                    }
                 }
 
             }
 
 
-        }else{
+        } else {
             binding.passwordInputCardStudentProfileFragment.visibility = View.GONE
             binding.updateButtonStudentProfileFragment.visibility = View.GONE
 
@@ -172,7 +178,7 @@ class StudentProfileFragment: Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        if(this::studentLive.isInitialized) studentLive.removeObservers(viewLifecycleOwner)
+        if (this::studentLive.isInitialized) studentLive.removeObservers(viewLifecycleOwner)
     }
 
     private fun isDigitsOnly(str: String): Boolean {
